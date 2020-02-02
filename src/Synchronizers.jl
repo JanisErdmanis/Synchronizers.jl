29c063497f0a681918b90d25316c426d84d08147
akels <akels14@gmail.com>
2020-02-02 22:32:33 +0100

    prep for release

--- src/Synchronizers.jl
+++ src/Synchronizers.jl
@@ -1,5 +1,7 @@
 module Synchronizers
 
+using Sockets
+
 function stack(io::IO,msg::Vector{UInt8})
     frontbytes = reinterpret(UInt8,Int16[length(msg)])
     item = UInt8[frontbytes...,msg...]
@@ -17,8 +19,8 @@ function unstack(io::IO)
     return msg
 end
 
-stack(io::IO,n::Int) = stack(io,reinterpret(UInt8,Int[n]))
-unstack(io::IO,x::Type{Int}) = reinterepret(Int,unstack(io))[1]
+stack(io::IO,n::Int) = stack(io,UInt8[reinterpret(UInt8,Int[n])...])
+unstack(io::IO,x::Type{Int}) = reinterpret(Int,unstack(io))[1]
 
 
 struct Record
@@ -26,14 +28,18 @@ struct Record
     data::Vector{UInt8}
 end
 
+import Base.==
+==(a::Record,b::Record) = a.fname==b.fname && a.data==b.data
+
+
 function stack(socket::IO,record::Record)
-    binary = UInt8[record.fname...,'\n',record.data...]
+    binary = UInt8[record.fname...,UInt8('\n'),record.data...]
     stack(socket,binary)
 end
 
 function unstack(socket::IO,x::Type{Record})
     binary = unstack(socket)
-    n = findfirst(x->x=='\n',binary)
+    n = findfirst(x->x==UInt8('\n'),binary)
     fname = String(binary[1:n-1])
     data = binary[n+1:end]
     return Record(fname,data)
@@ -41,32 +47,55 @@ end
 
 ### I could test whether the stuff works for the buffer.
 
-
 mutable struct Ledger
     dir::AbstractString
     records::Vector{Record}
 end
 
+==(a::Ledger,b::Ledger) = a.records==b.records
+
 import Base.length
 length(ledger::Ledger) = length(ledger.records)
 
-function Ledger(dir::AbstractString)
-    records = Record[]
+function readfile(fname::AbstractString)
+    file = open(fname,"r") 
+    data = UInt8[]
+    while !eof(file)
+        push!(data,read(file,UInt8))
+    end
+    close(file)
+    return data
+end
 
-    ### I actually need to sort them by date
-    for fname in readdir(dir)
-        data = readall(fname)
-        record = Record(fname,data)
-        push!(records,record)
+function Ledger(rootdir::AbstractString)
+    mkpath(rootdir)
+    records = Record[]
+    time = []
+
+    ### I actually need to sort them by date 
+    for (dir, _, files) in walkdir(rootdir)
+        for fname in files
+            fullpath = "$dir/$fname"
+            data = readfile(fullpath)
+            record = Record(relpath(fullpath,rootdir),data)
+            push!(records,record)
+            push!(time,ctime(fullpath))
+        end
+    end
+    
+    if length(records)>1
+        sp = sortperm(time)
+        records = records[sp]
     end
 
-    return Ledger(dir,records)
+    return Ledger(rootdir,records)
 end
 
 ### One can latter extend this method to specify what to do with a specific type
 import Base.push!
 function push!(l::Ledger,r::Record)
-    push!(l.items,r)
+    push!(l.records,r)
+    mkpath(dirname(l.dir * r.fname))
     write(l.dir * r.fname,r.data)
 end
 
@@ -82,7 +111,7 @@ function serve(port,ledger::Ledger)
             stack(socket,n)
 
             while true
-                m = parse(Int,unstack(socket))
+                m = unstack(socket,Int)
                 if m==0
                     n = length(ledger)::Int
                     stack(socket,n)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,48 @@
 using Synchronizers
+using Synchronizers: stack, unstack, Record, Ledger, serve, isready,  Synchronizer, sync
+using Test
 
-ledger = ["hello", "world", "this", "is", "synchronizer"]
+### Testing Int
 
-master = Synchronizer(server,ledger) # 
+io = IOBuffer()
+stack(io,123)
 
-slaveledger = []
-slave = Synchronizer(socket,slaveledger,:slave)
+ionew = IOBuffer(take!(io))
+@test unstack(ionew,Int)==123
 
+### Testing Record
 
+record = Record("hello",b"world")
+
+io = IOBuffer()
+stack(io,record)
+
+ionew = IOBuffer(take!(io))
+@test unstack(ionew,Record)==record
+
+### Testing now the ledger
+masterdir = dirname(@__FILE__) * "/master/"
+
+mledger = Ledger(masterdir)
+push!(mledger,Record("hellohere",b"world"))
+
+# ### Now let's test synchronization
+
+task = @async serve(2000,mledger)
+
+sleep(1)
+
+slavedir = dirname(@__FILE__) * "/slave/"
+sledger = Ledger(slavedir)
+
+s = Synchronizer(2000,sledger)
+
+@test isready(s)==true
+
+sync(s)
+
+@test mledger==sledger
+
+# WIP IO is too fast
+#sledger2 = Ledger(sledger)
+#@test sledger==sledger2
